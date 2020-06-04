59bfa17d124dd63f29570ab70e2b24146ee6b8c9
A01610805 <65374426+A01610805@users.noreply.github.com>
2020-06-04 18:45:45 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,30 +10,51 @@
 #include "nota.h"
 using namespace std;
 
-//  Este main es un ejemplo de como obtener cada uno de los tipos de acordes incluidos en el programa. Posteriormente se creará otra clase "cancion", donde almacenaremos uno a uno los acordes que el usuario requiera para ir construyendo su canción
-
 int main() {
-    nota n;
-    int numnot = n.setNota();
-    n.creaAcorde(numnot);   // Aqui nos imprime la quinta; como no se define ninguna clase hereditaria, se lleva a cabo la función creaAcorde dentro de nota
     
-    nota *M = new Mayor();
-    M -> creaAcorde(numnot);    // Aqui obtenemos un acorde mayor, ya que definimos el apuntador M como un nuevo objeto de la clase Mayor
+    string cancion; // Se declaran variables
+    nota nt;
+    cout << "Acorde 1" << endl;
+    int num = nt.setNota();
+    int n = 0;
+    Acorde *acordes[99];
     
-    nota *m = new Menor();
-    m -> creaAcorde(numnot);    // Aqui obtenemos un acorde menor, ya que definimos el apuntador m como un nuevo objeto de la clase Menor
+    while (num != 12){  // mun será igual a 12 cuando el usuario teclee "Fin" (ver función setNota)
+        int type = 6;   // Para entrar en el while. Esta variable cambiará de valor una vez dentro.
+        while (1 > type or type > 5){
+        type = nt.ChordType();
+            if (type == 1){
+               acordes[n] = new Mayor();    // Se crea nuevo objeto "Mayor"
+            }
+            else if (type == 2){
+                acordes[n] = new Menor();   // Se crea nuevo objeto "Menor"
+            }
+            else if (type == 3){
+                acordes[n] = new Dism();    // Se crea nuevo objeto "Dism"
+            }
+        
+            else if (type == 4){
+               acordes[n] = new D7();     // Se crea nuevo objeto "D7"
+            }
+        
+            else if (type == 5){
+                acordes[n] = new Maj7();    // Se crea nuevo objeto "Maj7"
+            }
+            else{
+                cout << "Por favor introduzca un número del 1 al 5" << endl;
+            }
+        }
+        string acorde = acordes[n] -> creaAcorde(num);  // Se crea un acorde de acuerdo al objeto creado
+        string c = acordes[n] -> compas();  // El usuario agrega la duración del acorde
+        cancion = cancion + "\n" + acorde + c;  //  Aqui se va guardando la canción en la variable "cancion", sumando los strings obtenidos en cada vuelta del ciclo.
+        n++;
+        cout <<"\n" <<  "Acorde " << n+1 << endl;   //Pedimos el siguiente acorde.
+        num = nt.setNota();
+        
+    }
     
-    nota *D = new Dism();
-    D -> creaAcorde(numnot);    // Aqui obtenemos un acorde disminuido, ya que definimos el apuntador D como un nuevo objeto de la clase Disminuido
-    
-    nota *S = new A7();
-    S -> creaAcorde(numnot);    // Aqui obtenemos un acorde dominante 7, ya que definimos el apuntador S como un nuevo objeto de la clase A7
-    
-    delete M;
-    delete m;
-    delete D;
-    delete S;
-    
+    cout << cancion << endl; // Una vez que el usuario teclea "FIn", se imprimen los acordes almacenados
+
     
     return 0;
 }
